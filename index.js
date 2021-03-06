b49e1cd524660e3e0e400b89004a5204218af681
Ahmed Wahba <>
2021-03-06 14:49:42 +0300

    local notification configuration

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import { registerRootComponent } from 'expo';
+import { Alert } from 'react-native';
 
 import App from './App';
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import NotificationService from './src/utilities/NotificationService';
+
+window.notification = new NotificationService(onNotification);
+
+const onNotification = (notif) => {
+    Alert.alert(notif.title, notif.message);
+    console.log('notification', notif);
+    window.notification.finish(PushNotificationIOS.FetchResult.NoData);
+};
+
+    //Permissions to use notifications
+function handlePerm(perms) {
+    Alert.alert("Permissions", JSON.stringify(perms));
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
